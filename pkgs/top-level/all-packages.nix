cc81458fbe5e528cd74cc72ed5e8de1317426975
Raito Bezarius <masterancpp@gmail.com>
2023-06-19 02:39:30 +0200

    systemdMinimal: expose `withRepart` flag and disable it
    systemd-repart relies on cryptsetup to perform LUKS operations,
    therefore, it's not suitable for systemdMinimal.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28435,6 +28435,7 @@ with pkgs;
     withCompression = false;
     withCoredump = false;
     withCryptsetup = false;
+    withRepart = false;
     withDocumentation = false;
     withEfi = false;
     withFido2 = false;
